c92a9ff0e1eb48251bd58854d5093ca11618ab62
paopaot <yaojinfeng23@sina.com>
2021-09-03 23:28:59 +0800

    badge

--- components/badge/badge.component.ts
+++ components/badge/badge.component.ts
@@ -15,11 +15,18 @@ import { PaoBadgeColor, PaoBadgePosition, PaoBadgeSize } from './type';
 @Component({
 	selector: '[paoBadge]',
 	template: `
-		<span class="pao-badge">
-			<ng-content></ng-content>
-			<span class="pao-badge-content">0</span>
-		</span>
+		<ng-content></ng-content>
+		<span class="pao-badge-content" *ngIf="!paoBadgeHidden" [class.disabled]="paoBadgeDisabled">{{paoBadge}}</span>
 	`,
+	host: {
+		'[class.pao-badge-small]': "paoBadgeSize === 'small'",
+		'[class.pao-badge-medium]': "paoBadgeSize === 'medium'",
+		'[class.pao-badge-large]': "paoBadgeSize === 'large'",
+		'[class.pao-badge-primary]': "paoBadgeColor === 'primary'",
+		'[class.pao-badge-accent]': "paoBadgeColor === 'accent'",
+		'[class.pao-badge-warn]': "paoBadgeColor === 'warn'",
+		'[class.pao-badge-overlap]': 'paoBadgeOverlap',
+	},
 	styleUrls: ['./badge.component.less'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	encapsulation: ViewEncapsulation.None
@@ -29,19 +36,34 @@ export class PaoBadgeComponent implements OnInit, OnChanges {
 	@Input() paoBadgeOverlap = false;
 	@Input() paoBadgeDisabled = false;
 	@Input() paoBadgeHidden = false;
-	@Input() paoBadgeColor?: PaoBadgeColor;
+	@Input() paoBadgeColor?: PaoBadgeColor = 'primary';
 	@Input() paoBadgeSize: PaoBadgeSize = 'medium';
 	@Input() paoBadgePosition: PaoBadgePosition = 'above after';
+
+	cls = '';
 
 	constructor(
 		private eleRef: ElementRef,
 		private cdr: ChangeDetectorRef,
-		renderer2: Renderer2
-	) { }
+		private renderer2: Renderer2
+	) {
+		this.renderer2.addClass(this.eleRef.nativeElement, 'pao-badge');
+	}
 
-	ngOnChanges(changes: SimpleChanges) {}
+	ngOnChanges(changes: SimpleChanges) {
+		if (changes.paoBadgePosition) {
+			this.setBadgePosition();
+		}
+	}
 
 	ngOnInit() {
-		console.log(123);
+		this.setBadgePosition();
+	}
+
+	setBadgePosition() {
+		this.paoBadgePosition.split(' ').forEach(v => {
+			this.renderer2.addClass(this.eleRef.nativeElement, 'pao-badge-' + v);
+		});
+		this.cdr.markForCheck();
 	}
 }
--- components/badge/badge.module.ts
+++ components/badge/badge.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '_@angular_common@12.2.1@@angular/common';
 
 import { PaoBadgeComponent } from './badge.component';
 
 @NgModule({
-	imports: [],
+	imports: [CommonModule],
 	exports: [PaoBadgeComponent],
 	declarations: [PaoBadgeComponent],
 	providers: [],
