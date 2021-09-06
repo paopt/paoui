c4aecb716a42148659e157c044e16750d49c9a6a
paopaot <yaojinfeng23@sina.com>
2021-09-07 01:31:55 +0800

    card初始化

---
+++ components/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, ViewEncapsulation, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'pao-card',
+  template: '',
+  styleUrls: ['./card.component.less'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PaoCardComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ components/card/card.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { PaoCardComponent } from './card.component';
+
+@NgModule({
+  imports: [],
+  exports: [PaoCardComponent],
+  declarations: [PaoCardComponent],
+  providers: [],
+})
+export class NameModule { }
