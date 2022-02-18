a8d595227df62c617a99ca52c779ecc7c8661559
paopaot <yaojinfeng23@sina.com>
2022-02-18 08:41:31 +0800

    slider

--- components/slider/slider.component.ts
+++ components/slider/slider.component.ts
@@ -2,7 +2,6 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
-  OnInit,
   Output,
   ViewEncapsulation,
   EventEmitter,
@@ -11,19 +10,25 @@ import {
   OnDestroy,
   AfterViewInit,
   NgZone,
-  Renderer2,
   Provider,
   forwardRef,
   ViewChild,
+  Attribute,
 } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import {
   coerceBooleanProperty,
   NumberInput,
   BooleanInput,
+  coerceNumberProperty,
 } from '@angular/cdk/coercion';
 import { FocusMonitor } from '@angular/cdk/a11y';
-import { UP_ARROW, DOWN_ARROW, LEFT_ARROW, RIGHT_ARROW } from '@angular/cdk/keycodes';
+import {
+  UP_ARROW,
+  DOWN_ARROW,
+  LEFT_ARROW,
+  RIGHT_ARROW,
+} from '@angular/cdk/keycodes';
 
 import { PaoColor } from '../core/type';
 
@@ -32,53 +37,76 @@ let uniqueId = 0;
 const CONTROL: Provider = {
   provide: NG_VALUE_ACCESSOR,
   useExisting: forwardRef(() => PaoSliderComponent),
-  multi: true
-}
+  multi: true,
+};
 
 @Component({
   selector: 'pao-slider',
   exportAs: 'paoSlider',
   templateUrl: 'slider.component.html',
   styleUrls: ['./slider.component.less'],
-  providers: [
-    CONTROL
-  ],
+  providers: [CONTROL],
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     '[attr.id]': 'id',
     '[attr.name]': 'name',
-    '[attr.tabIndex]': 'tabIndex',
+    '[tabIndex]': 'tabIndex',
     'class': 'pao-slider',
     '[class.pao-primary]': 'color === "primary"',
     '[class.pao-accent]': 'color === "accent"',
     '[class.pao-warn]': 'color === "warn"',
     '[class.pao-slider-disabled]': 'disabled',
-    '[class.pao-slider-vertical]': 'vertical',
-    '[class.pao-slider-invert]': 'invert',
     '[class.pao-slider-thumbLabel]': 'thumbLabel',
-    '[class.pao-slider-active]': 'value > 0'
+    '[class.pao-slider-active]': 'value > 0',
   },
 })
-export class PaoSliderComponent
-  implements OnInit, ControlValueAccessor, OnDestroy, AfterViewInit {
+export class PaoSliderComponent implements ControlValueAccessor, OnDestroy, AfterViewInit {
   @Input() id = `pai-slider-${++uniqueId}`;
 
   @Input() name: string | null = null;
 
   @Input() color: PaoColor = 'primary';
 
-  @Input() min: number = 0;
+  @Input() displayWith!: (value: number) => string | number;
 
-  @Input() max: number = 100;
+  @Input()
+  set min(value: number) {
+    this._min = coerceNumberProperty(value, this._min);
+    if (this.value === null) {
+      this.value = this._min;
+    }
+    this._percent = this._calculatePercent(this.value);
+    this.cdr.markForCheck();
+  }
+  get min(): number {
+    return this._min;
+  }
+  private _min: number = 0;
 
-  @Input() step: number = 1;
+  @Input()
+  set max(value: number) {
+    this._max = coerceNumberProperty(value, this._max);
+    this._percent = this._calculatePercent(this.value);
+    this.cdr.markForCheck();
+  }
+  get max() {
+    return this._max;
+  }
+  private _max = 100;
 
-  @Input() tickInterval: number = 0;
-
-  @Input() tabIndex: number = 0;
-
-  @Input() displayWith: (value: number) => string | number = (value) => value;
+  @Input()
+  set step(value: number) {
+    this._step = coerceNumberProperty(value, this._step);
+    if (this._step % 1 !== 0) {
+      this._roundDecimal = this._step.toString().split('.').pop()!.length;
+    }
+  }
+  get step(): number {
+    return this._step;
+  }
+  private _step = 1;
+  private _roundDecimal!: number;
 
   @Input()
   set disabled(value: any) {
@@ -90,15 +118,6 @@ export class PaoSliderComponent
   private _disabled = false;
 
   @Input()
-  set invert(value: boolean) {
-    this._invert = coerceBooleanProperty(value);
-  }
-  get invert(): boolean {
-    return this._invert;
-  }
-  private _invert = false;
-
-  @Input()
   set thumbLabel(value: boolean) {
     this._thumbLabel = coerceBooleanProperty(value);
   }
@@ -108,75 +127,83 @@ export class PaoSliderComponent
   private _thumbLabel = false;
 
   @Input()
-  set vertical(value: boolean) {
-    this._vertical = coerceBooleanProperty(value);
-  }
-  get vertical() {
-    return this._vertical;
-  }
-  private _vertical = false;
-
-  @Input()
-  set value(val: number) {
-    this._value = val;
+  set value(value: number) {
+    if (value !== this._value) {
+      let val = coerceNumberProperty(value);
+      if (this._roundDecimal && val !== this.min && val !== this.max) {
+        val = parseFloat(val.toFixed(this._roundDecimal));
+      }
+      this._value = val;
+      this._percent = this._calculatePercent(this._value);
+      console.log(this.percent);
+      this.cdr.markForCheck();
+    }
   }
   get value(): number {
-    return this._value;
+    if (this._value === null) {
+      return this.min;
+    }
+    return this.min;
   }
   private _value: number = 0;
 
-  @Output() change = new EventEmitter();
+  get percent(): number {
+    return this._clamp(this._percent);
+  }
+  private _percent = 0;
+
+  get displayValue(): string | number {
+    if (this.displayWith) {
+      return this.displayWith(this.value);
+    }
+    if (this._roundDecimal && this.value && this.value % 1 !== 0) {
+      return this.value.toFixed(this._roundDecimal);
+    }
+    return this.value || 0;
+  }
+
   @Output() input = new EventEmitter();
+  @Output() change = new EventEmitter();
+  @Output() valueChange = new EventEmitter();
 
-  @ViewChild('thumb') thumbEleRef!: ElementRef;
-  @ViewChild('track') trackEleRef!: ElementRef;
-  @ViewChild('ball') ballEleRef!: ElementRef;
+  @ViewChild('sliderWrapper') sliderWrapper!: ElementRef;
 
-  private onChange: (value: any) => void = () => {};
-  private onTouched: () => any = () => {};
+  private _onChange: (value: any) => void = () => {};
+  private _onTouched: () => any = () => {};
 
-  private isDragging = false;
-  private startX: number = 0;
-  private thumbX: number = 0;
-  private hasFocus = false;
+  tabIndex: number = 0;
+
+  private _isSliding = false;
+  private _startSlide!: number | null;
 
   constructor(
     private cdr: ChangeDetectorRef,
     private focusMonitor: FocusMonitor,
     private elementRef: ElementRef,
     private ngZone: NgZone,
-    private renderer2: Renderer2
+    @Attribute('tabindex') tabIndex: string
   ) {
+    this.tabIndex = parseFloat(tabIndex) || 0;
+    // 注册事件
+    this.ngZone.runOutsideAngular(() => {
+      const ele = this.elementRef.nativeElement;
+      ele.addEventListener('focus', this._focus);
+      ele.addEventListener('blur', this._blur);
+      ele.addEventListener('keydown', this._onKeyDown);
+      ele.addEventListener('keyup', this._onKeyUp);
+
+      ele.addEventListener('mousedown', this._onMouseDown);
+    });
   }
 
-  ngOnInit() {
-  }
-
-  /**
-   * 绑定事件
-   */
   ngAfterViewInit(): void {
-    this.thumbX = this.thumbEleRef.nativeElement.getBoundingClientRect().left;
-    // 焦点监听
     this.focusMonitor
       .monitor(this.elementRef.nativeElement, true)
       .subscribe((focusOrigin) => {
-        if (focusOrigin) {
-          this.hasFocus = true;
-        } else {
-          this.hasFocus = false;
-          this.onTouched();
+        if (!focusOrigin) {
+          this._onTouched();
         }
       });
-
-    // 注册事件
-    this.ngZone.runOutsideAngular(() => {
-      this.elementRef.nativeElement.addEventListener('click', this._onClick);
-      this.ballEleRef.nativeElement.addEventListener('mousedown', this._onMousedown);
-      document.addEventListener('mousemove', this._onMousemove);
-      document.addEventListener('mouseup', this._onMouseup);
-      document.addEventListener('keydown', this._onKeydown);
-    });
   }
 
   ngOnDestroy(): void {
@@ -189,109 +216,141 @@ export class PaoSliderComponent
   }
 
   registerOnChange(fn: any): void {
-    this.onChange = fn;
+    this._onChange = fn;
   }
 
   registerOnTouched(fn: any): void {
-    this.onTouched = fn;
+    this._onTouched = fn;
   }
 
   setDisabledState(isDisabled: boolean) {
     this.disabled = isDisabled;
   }
 
-  /**
-   * 处理mousedown事件， 拖动开始
-   * @param event 
-   */
-  private _onMousedown = (event: MouseEvent) => {
-    event.stopPropagation();
-    this.isDragging = true;
-    this.startX = event.clientX;
-  }
-
-  /**
-   * 处理mousemove事件, 拖动中
-   * @param event 
-   */
-  private _onMousemove = (event: MouseEvent) => {
-    if (this.isDragging) {
-      const dis = event.clientX - this.startX;
-      this.startX = event.clientX;
-      const rect = this.thumbEleRef.nativeElement.getBoundingClientRect();
-      const distance = rect.left - this.thumbX + dis;
-      const percent = (distance / rect.width);
-      console.log(percent);
-      this.updateValue(percent);
-    //   console.log(this.value);
-    //   // let percent = (this.value / this.max) * 100;
-    // this.thumbEleRef.nativeElement.style.transform = `translateX(${percent}%)`;
-    // this.onChange(this.value);
-    // this.value = Math.ceil((this.max * percent) / this.step) * this.step;
-    }
-  }
-
-  /**
-   * 处理mouseup事件， 拖动结束
-   * @param event 
-   */
-  private _onMouseup = (event: any) => {
-    this.isDragging = false;
-  }
-
-  /**
-   * 处理点击事件
-   * @param event 
-   */
-  private _onClick = (event: MouseEvent) => {
-    const rect = this.trackEleRef.nativeElement.getBoundingClientRect();
-    const { width, left } = rect;
-    const percent = (event.clientX - left) / width;
-    this.updateValue(percent);
-  }
-
-  /**
-   * 获得焦点时，处理键盘事件
-   * @param event 
-   */
-  private _onKeydown = (event: KeyboardEvent) => {
-    if (!this.hasFocus) {
+  private _onKeyDown = (event: KeyboardEvent) => {
+    if (this.disabled) {
       return;
     }
 
+    this._isSliding = true;
+
     const keyCode = event.keyCode;
+    const oldValue = this.value;
 
-    // value减少一步
     if (keyCode === DOWN_ARROW || keyCode === LEFT_ARROW) {
-      this.value -= this.step;
+      this._increment(-1);
     }
-    
-    // value增加一步
+
     if (keyCode === UP_ARROW || keyCode === RIGHT_ARROW) {
-      this.value += this.step;
+      this._increment(1);
     }
-    this.updateSliderThumb();
+
+    if (oldValue !== this.value) {
+      this.input.emit(this.value);
+      this.change.emit(this.value);
+      this.valueChange.emit(this.value);
+      this._onChange(this.value);
+    }
   }
 
-  /**
-   * 根据百分比计算当前值
-   * @param percent 
-   */
-  private updateValue(percent: number) {
-    this.value = Math.round((this.max * percent) / this.step) * this.step;
-    this.updateSliderThumb();
+  private _onKeyUp = () => {
+    this._isSliding = false;
   }
 
-  /**
-   * 根据value值，更新滑块位置
-   */
-  private updateSliderThumb() {
-    this.value = Math.max(this.min, this.value);
-    this.value = Math.min(this.max, this.value);
-    let percent = (this.value / this.max) * 100;
-    this.thumbEleRef.nativeElement.style.transform = `translateX(${percent}%)`;
-    this.onChange(this.value);
-    this.cdr.markForCheck();
+  private _updateValue = (event: MouseEvent) => {
+    const x = event.clientX;
+    const rect = this.sliderWrapper.nativeElement.getBoundingClientRect();
+    const percent = this._clamp((x - rect.left) / rect.width);
+    if (percent === 0) {
+      this.value = this.min;
+    } else if (percent === 1) {
+      this.value = this.max;
+    } else {
+      const exactValue = this.min + percent * (this.max - this.min);
+      const closetValue = Math.round((exactValue - this.min) / this.step) * this.step + this.min;
+      this.value = this._clamp(closetValue, this.min, this.max);
+    }
+  }
+
+  private _onMouseDown = (event: MouseEvent) => {
+    if (this.disabled || this._isSliding) {
+      return;
+    }
+
+    this._isSliding = true;
+
+    this._updateValue(event);
+
+    this._startSlide = this.value;
+    this.cdr.detectChanges();
+
+    document.addEventListener('mousemove', this._onMouseMove);
+    document.addEventListener('mouseup', this._onMouseUp);
+  }
+
+  private _onMouseMove = (event: MouseEvent) => {
+    if (this._isSliding) {
+      const oldValue = this.value;
+      this._updateValue(event);
+      if (oldValue !== this.value) {
+       this.input.emit(this.value);
+     }
+    }
+  }
+
+  private _onMouseUp = (event: MouseEvent) => {
+    if (this._isSliding) {
+      if (this.value !== this._startSlide) {
+        this._onChange(this.value);
+        this.change.emit(this.value);
+        this.valueChange.emit(this.value);
+      }
+      
+      this._isSliding = false;
+      this._startSlide = null;
+
+      // 删除事件
+      document.removeEventListener('mousemove', this._onMouseMove);
+      document.removeEventListener('mouseup', this._onMouseUp);
+    }
+  }
+
+  private _focus = () => {
+    this.elementRef.nativeElement.focus();
+  }
+
+  private _blur = () => {
+    this.elementRef.nativeElement.blur();
+  }
+
+  private _increment(nums: number) {
+    this.value = this._clamp(this.value + nums * this.step, this.min, this.max);
+  }
+
+  private _clamp(value: number, min = 0, max = 1) {
+    return Math.max(min, Math.min(value, max));
+  }
+
+  private _calculatePercent(value: number | null) {
+    return ((value || 0) - this.min) / (this.max - this.min);
+  }
+
+  getTrackBackgroundStyle() {
+    return {
+      'transform': `scale3d(${1-this.percent}, 1, 1)`
+    };
+  }
+
+  getTrackFillStyle() {
+    return {
+      'transform': `scale3d(${this.percent}, 1, 1)`
+    };
+  }
+
+  getThumbStyle() {
+    return {
+
+    };
   }
 
   static ngAcceptInputType_min: NumberInput;
--- components/slider/slider.module.ts
+++ components/slider/slider.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PaoSliderComponent } from './slider.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   exports: [PaoSliderComponent],
   declarations: [PaoSliderComponent],
   providers: [],
